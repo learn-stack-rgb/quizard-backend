888039ce0304e50503d6691a949817d98f6a2b80
Graham <blundell.gp@gmail.com>
2023-09-20 14:52:00 -0700

    added read and rspec test

--- spec/requests/decks_spec.rb
+++ spec/requests/decks_spec.rb
@@ -1,7 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "Decks", type: :request do
+  let(:user) {User.create(
+    email: 'Deck1@deck1.mail',
+    password: 'password'
+  )}
+ 
+  
+  
+  
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a list of decks" do
+      deck = user.decks.create(
+        title: 'Deck1'
+      )
+
+      get '/decks'
+
+      deck = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(deck.first['title']). to eq('Deck1')
+    end
   end
 end
