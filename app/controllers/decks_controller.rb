2331cfad90001d3df58ff823cc6ca32bdc4536d0
Bao Khanh Tran <ktbv.22@gmail.com>
2023-09-20 14:27:21 -0700

    added backend Create, Update, and Delete Tests and Functionality

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,2 +1,38 @@
 class DecksController < ApplicationController
+  def index
+  end
+
+  def create
+    deck = Deck.create(deck_params)
+    if deck.valid? 
+      render json: deck
+    else
+      render json: deck.errors, status: 422
+    end
+  end
+
+  def update
+    deck = Deck.find(params[:id])
+    deck.update(deck_params)
+    if deck.valid?
+      render json: deck
+    else 
+      render json: deck.errors, status: 422
+    end
+  end
+
+  def destroy
+    deck = Deck.find(params[:id])
+    deck.destroy
+    if deck.valid?
+      render json: deck
+    else
+      render json: deck.errors, status: 422
+    end
+  end
+
+  private
+  def deck_params
+    params.require(:deck).permit(:title, :user_id)
+  end
 end
