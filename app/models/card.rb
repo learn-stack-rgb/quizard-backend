91146a764b0c5d6e6cf079afbf30036cfeab2295
Bao Khanh Tran <ktbv.22@gmail.com>
2023-09-26 09:18:40 -0700

    adjusted validations for front-end create to work properly

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,4 @@
 class Card < ApplicationRecord
   belongs_to :deck
-  validates :question, :answer, :seen, :correct, :deck_id, presence: true
+  validates :question, :answer, :deck_id, presence: true
 end
